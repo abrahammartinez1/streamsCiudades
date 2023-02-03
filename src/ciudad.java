9c40c1de5394c6837ce7711af72a21df9a185a1e
abranllis <47560487+abranllis@users.noreply.github.com>
2023-02-03 14:28:42 +0100

    12

--- src/ciudad.java
+++ src/ciudad.java
@@ -1,15 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
 class ciudad {
-    private String nombre;
-    private String provincia;
-    private int habitantes;
-    private int extension;
+    private final String nombre;
+    private final String provincia;
+    private final int habitantes;
+    private final int extension;
 
-    public Ciudad(String nombre, String provincia, int habitantes, int extension) {
+    public ciudad(String nombre, String provincia, int habitantes, int extension) {
         this.nombre = nombre;
         this.provincia = provincia;
         this.habitantes = habitantes;
@@ -33,28 +28,3 @@ class ciudad {
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        List<Ciudad> ciudades = new ArrayList<>();
-        ciudades.add(new Ciudad("Madrid", "Madrid", 3000000, 600));
-        ciudades.add(new Ciudad("Barcelona", "Barcelona", 2000000, 400));
-        ciudades.add(new Ciudad("Valencia", "Valencia", 800000, 300));
-
-        // De cuántas provincias diferentes son las ciudades?
-        long numeroProvincias = ciudades.stream().map(Ciudad::getProvincia).distinct().count();
-        System.out.println("Numero de provincias diferentes: " + numeroProvincias);
-
-        // ¿Cuántas ciudades hay?
-        System.out.println("Numero de ciudades: " + ciudades.size());
-
-        // Calcula el número total de habitantes para una provincia determinada (introducida por el usuario, por ejemplo)
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce una provincia: ");
-        String provincia = sc.nextLine();
-        int totalHabitantes = ciudades.stream().filter(c -> c.getProvincia().equals(provincia)).mapToInt(Ciudad::getHabitantes).sum();
-        System.out.println("Numero total de habitantes en " + provincia + ": " + totalHabitantes);
-
-        // Obtén una colección con los nombres de todas las ciudades
-        List<String> nombresCiudades = ciudades.stream().map(Ciudad::getNombre).collect(Collectors.toList());
-        System.out.println("Nombres de las ciudades: " + nombresCiudad);
-    }
---
+++ src/main.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<ciudad> ciudades = new ArrayList<>();
+        ciudades.add(new ciudad("Madrid", "Madrid", 3000000, 600));
+        ciudades.add(new ciudad("Barcelona", "Barcelona", 2000000, 400));
+        ciudades.add(new ciudad("Valencia", "Valencia", 800000, 300));
+
+        // De cuántas provincias diferentes son las ciudades?
+        long numeroProvincias = ciudades.stream().map(ciudad::getProvincia).distinct().count();
+        System.out.println("Numero de provincias diferentes: " + numeroProvincias);
+
+        // ¿Cuántas ciudades hay?
+        System.out.println("Numero de ciudades: " + ciudades.size());
+
+        // Calcula el número total de habitantes para una provincia determinada (introducida por el usuario, por ejemplo)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce una provincia: ");
+        String provincia = sc.nextLine();
+        int totalHabitantes = ciudades.stream().filter(c -> c.getProvincia().equals(provincia)).mapToInt(ciudad::getHabitantes).sum();
+        System.out.println("Numero total de habitantes en " + provincia + ": " + totalHabitantes);
+
+        // Obtén una colección con los nombres de todas las ciudades
+        List<String> nombresCiudades = ciudades.stream().map(ciudad::getNombre).collect(Collectors.toList());
+        System.out.println("Nombres de las ciudades: " + nombresCiudades);
+    }
+}
